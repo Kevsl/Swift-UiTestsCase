a867f3055c9433c184dd3e0b1617c61296d43dea
Kevsl <ktr2@pm.me>
2023-10-10 00:12:20 +0200

    Initial Commit

---
+++ uiTests/uiTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  uiTestsApp.swift
+//  uiTests
+//
+//  Created by K on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct uiTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
